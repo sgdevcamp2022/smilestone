f4c61d7ac535101944b1cf029d7c339f2470472b
jeongjin984 <philipwq3963@gmail.com>
2023-02-19 17:05:17 +0900

    m

--- src/pages/testChat/TestChat.tsx
+++ src/pages/testChat/TestChat.tsx
@@ -2,36 +2,31 @@ import SockJS from "sockjs-client"
 import Stomp from "stompjs"
 import {ChangeEvent, useState} from "react";
 
-const stompClient = Stomp.over(new SockJS("http://localhost:8080/smilestone/chat"))
-stompClient.connect('test', 'test', function (frame) {
-  console.log('STOMP Connected');
-}, (e) => console.error(e), "/");
+const stompClient = Stomp.over(new SockJS("http://3.34.86.115:8090/smilestone/chat"))
+stompClient.connect({}, (frame) => {}, (e) => {});
 
 export default function TestChat() {
   const [state, setState] = useState("close")
   const [username, setUsername] = useState("tester")
-  const [roomId, setRoomId] = useState("testRoom")
-  const [roomId2, setRoomId2] = useState("testRoom2")
+  const [roomId, setRoomId] = useState("0")
+  const [roomId2, setRoomId2] = useState("1")
   const [message, setMessage] = useState("")
   const [message2, setMessage2] = useState("")
   const [messageHistory, setMessageHistory] = useState<any>([])
   const [messageHistory2, setMessageHistory2] = useState<any>([])
 
   const onClickTestChatRoom = () => {
-    stompClient.subscribe(`/exchange/chat.exchange/chatroom.${roomId}`, (msg) => {
+    stompClient.subscribe(`/chat/${roomId}`, (msg) => {
       const jsonFrame = JSON.parse(msg.body)
       setMessageHistory((prev: any) => [...prev, {sender: jsonFrame.sender, message: jsonFrame.message, chatAt: jsonFrame.chatAt}])
-    },{'auto-delete':true, 'durable':false, 'exclusive':false});
-    stompClient.send(`/pub/chat.enter.${roomId}`, {}, JSON.stringify({roomId: roomId, sender: username, message: "tester Entered", chatAt: new Date().toISOString()}));
+    });
   }
 
   const onClickTestChatRoom2 = () => {
-    stompClient.subscribe(`/exchange/chat.exchange/chatroom.${roomId2}`, (msg) => {
+    stompClient.subscribe(`/chat/${roomId2}`, (msg) => {
       const jsonFrame = JSON.parse(msg.body)
       setMessageHistory2((prev: any) => [...prev, {sender: jsonFrame.sender, message: jsonFrame.message, chatAt: jsonFrame.chatAt}])
-    },{'auto-delete':true, 'durable':false, 'exclusive':false});
-    stompClient.send(`/pub/chat.enter.${roomId2}`, {}, JSON.stringify({roomId2: roomId, sender: username, message: "tester Entered", chatAt: new Date().toISOString()}));
-
+    },)
   }
 
   const onChangeMessage = (e: ChangeEvent<HTMLInputElement>) => {
@@ -43,11 +38,11 @@ export default function TestChat() {
   }
 
   const onClickSendMessage = () => {
-    stompClient.send(`/pub/chat.message.${roomId}`, {}, JSON.stringify({roomId: roomId, sender: username, message: message, chatAt: new Date().toISOString()}))
+    stompClient.send(`/pub/chat.${roomId}`, {}, JSON.stringify({roomId: roomId, sender: username, message: message, chatAt: new Date().toISOString()}))
   }
 
   const onClickSendMessage2 = () => {
-    stompClient.send(`/pub/chat.message.${roomId2}`, {}, JSON.stringify({roomId: roomId2, sender: username, message: message2, chatAt: new Date().toISOString()}))
+    stompClient.send(`/pub/chat.${roomId2}`, {}, JSON.stringify({roomId: roomId2, sender: username, message: message2, chatAt: new Date().toISOString()}))
   }
 
   return (
